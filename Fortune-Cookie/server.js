1bd72550c644782f0da04aadf21dbc69424ff186
LukeyBoyy <luke0307@hotmail.com>
2024-08-09 13:28:34 +1000

    added fortune messages

--- Fortune-Cookie/server.js
+++ Fortune-Cookie/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const child_process = require('child_process');
+const { error } = require('console');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    child_process.exec('fortune', (error, message) => {
+        if(error === null) {
+            res.send(message)
+        } else{
+            res.send('Error: ' + error);
+        } 
+    })
 });
 
 app.listen(3000, () => {
